10814d82096c296fab36c9cf02b13ce8f004d633
bext <jalbertomr@yahoo.com>
2019-12-08 17:50:05 -0600

    first commit generating reservations and saving on MongoDB

---
+++ src/main/java/com/bext/reservationservice/ReservationServiceApplication.java
@@ -0,0 +1,49 @@
+package com.bext.reservationservice;
+
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+class SamplaDataInitializr {
+	private final ReservationRepository reservationRepository;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void go() {
+
+		Flux<String> nombres = Flux.just("Hugo","Paco","Luis","Daisy","McPato","Beto","Guille","Juan");
+		Flux<Reservation> reservations = nombres.map(nombre -> new Reservation( null, nombre));
+		Flux<Reservation> saved = reservations.flatMap(this.reservationRepository::save);
+        saved.subscribe();
+	}
+}
+
+interface ReservationRepository extends ReactiveCrudRepository<Reservation, String> {
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document
+class Reservation {
+	@Id
+	private String id;
+	private String nombre;
+}
